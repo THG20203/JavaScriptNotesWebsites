9c13e8d61851dd8fdb6c0659a3566b2105c4a597
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-22 14:39:53 +0000

    notes made so far

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello World");
+/* Every value is an object or a primitive. Values are only
+a primitive if they are not an object.
+Example of an object:
+let me = {
+    name: "Jonas;
+}
+Example of primitive:
+let firstName= "Tristan";
+let age = 30;
+*/
